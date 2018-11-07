8cb0cfc226a715736223f89b70ab4c53f555b051
ecmichael97 <michaelcunningham197@gmail.com>
2018-11-07 18:13:33 -0500

    Remove ACE

--- Rematch.lua
+++ Rematch.lua
@@ -1,31 +1,10 @@
 local addonName, addonTable = ...;
+RematchAddon = { }
 
-Rematch = LibStub("AceAddon-3.0"):NewAddon("Rematch", "AceEvent-3.0")
-Rematch_Frame = LibStub("AceGUI-3.0")
 local combat_log = {}
 
-function Rematch:OnInitialize()
-Rematch:RegisterEvent("COMBAT_LOG_EVENT")
+local rematch_frame = Rematch_Frame
+rematch_frame.show()
 
-end
-
-
-function Rematch:OnEnable()
-
-
-end
-
-function Rematch:OnDisable()
-
-
-
-
-end
-
-function Rematch:COMBAT_LOG_EVENT(eventName,...)
-combat_log = ...
-Print(eventName .. ":" .. combat_log[13])
-
-end
 
 message(string.format("%s\nPost-Game information and statistics tailored for PvP Matches\nCurrently in development", addonName))
