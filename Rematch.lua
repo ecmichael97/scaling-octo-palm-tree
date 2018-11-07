22b4e8d43032bc45dbc02effa24f0d45aec373f4
ecmichael97 <michaelcunningham197@gmail.com>
2018-11-07 15:38:37 -0500

    Combat log event triggering
    barebones code to log a combat event

--- Rematch.lua
+++ Rematch.lua
@@ -1,3 +1,32 @@
 local addonName, addonTable = ...;
 
-message(string.format("%snPost-Game information and statistics tailored for PvP Matches\nCurrently in development", addonName))
+Rematch = LibStub("AceAddon-3.0"):NewAddon("Rematch", "AceEvent-3.0")
+Rematch:RegisterEvent("COMBAT_LOG_EVENT")
+Rematch_Frame = LibStub("AceGUI-3.0")
+local combat_log = {}
+
+function Rematch:OnInitialize()
+
+
+end
+
+
+function Rematch:OnEnable()
+
+
+end
+
+function Rematch:OnDisable()
+
+
+
+
+end
+
+function Rematch:COMBAT_LOG_EVENT(eventName,...)
+combat_log = ...
+Print(eventName .. ":" .. combat_log[13])
+
+end
+
+message(string.format("%s\nPost-Game information and statistics tailored for PvP Matches\nCurrently in development", addonName))
